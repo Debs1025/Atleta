3f69236996d632e8b41654686a476f03c9875d0f
GerardPelonio <gfpelonio@gbox.adnu.edu.ph>
2026-08-28 16:18:52 +0800

    refactor(structure): clean up root directory, keeping only Backend/ and Frontend/

--- scratch/verify_registry.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '../utils/firebaseAdmin';
-
-async function checkRegistry() {
-  const snap = await db.collection('Tournament_Registry').get();
-  console.log(`Tournament_Registry contains ${snap.size} entries:`);
-  snap.forEach(d => console.log(`- [${d.id}]: "${d.data().organization_name}" (Status: ${d.data().status})`));
-}
-
-checkRegistry().then(() => process.exit(0));
