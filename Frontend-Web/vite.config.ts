087431affcb243aa221df489f3defc419f35240e
GerardPelonio <gfpelonio@gbox.adnu.edu.ph>
2026-09-20 19:20:02 +0800

    fix(web): remove route interception in vite proxy

--- Frontend-Web/vite.config.ts
+++ Frontend-Web/vite.config.ts
@@ -5,23 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // Tailwind and React plugins
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/matches': {
-        target: 'https://atleta-backend.vercel.app/api/v1',
-        changeOrigin: true,
-        secure: true,
-      },
-      '/users': {
-        target: 'https://atleta-backend.vercel.app/api/v1',
-        changeOrigin: true,
-        secure: true,
-      },
-      '/api': {
-        target: 'https://atleta-backend.vercel.app',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 });
+
